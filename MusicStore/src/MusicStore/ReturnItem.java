a15775b107d9b02b5a7ffc894e77a3e1337ef673
William (Zach) Apple <zapple94@gmail.com>
2016-03-17 10:20:32 -0400

    ReturnItem work

--- MusicStore/src/MusicStore/ReturnItem.java
+++ MusicStore/src/MusicStore/ReturnItem.java
@@ -5,10 +5,44 @@
  */
 package MusicStore;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
 /**
  *
  * @author wlzapple, cabatts, ashalbert
  */
 class ReturnItem {
+    private final static int WIDTH = 350;
+    private final static int HEIGHT = 200;
+    
+    private JLabel receipt;
+    
+    private JButton yes, no;
+    
+    private YesButtonHandler yesBH;
+    private NoButtonHandler noBH;
+
+    private class NoButtonHandler implements ActionListener{
+
+        public NoButtonHandler() {
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+
+    private class YesButtonHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        
+    }
     
 }
